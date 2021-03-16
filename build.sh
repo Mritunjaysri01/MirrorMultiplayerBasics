ae730b09ddfe6d24bec4974b7e77279686fd6bf7
Mritunjaysri01 <mritunjaysri01@gmail.com>
2021-03-16 12:22:12 +0530

    initial

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2019.4.22f1\Editor\Unity.exe" -quit -batchmode -projectPath "F:\Unity Project\Ludo" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
